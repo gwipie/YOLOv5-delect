754c9cf31657019c4c92cae679782df009c568c3
UltralyticsAssistant <web@ultralytics.com>
2026-09-15 03:20:03 +0000

    Auto-format by https://ultralytics.com/actions

--- preprocess_640.py
+++ preprocess_640.py
@@ -1,17 +1,18 @@
+import glob
 import os
+
 import cv2
 import numpy as np
-import glob
 
 model_input_height = 640
 model_input_width = 640
 
-src_dir = '/data/datasets/prepare_data'
-dst_dir = '/data/yolov5/calibration_data_rgb_f32'
+src_dir = "/data/datasets/prepare_data"
+dst_dir = "/data/yolov5/calibration_data_rgb_f32"
 
 os.makedirs(dst_dir, exist_ok=True)
 
-image_files = glob.glob(os.path.join(src_dir, '*.jpg'))
+image_files = glob.glob(os.path.join(src_dir, "*.jpg"))
 
 for src_file in image_files:
     image = cv2.imread(src_file)
@@ -19,10 +20,10 @@ for src_file in image_files:
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image = np.transpose(image, (2, 0, 1))
     image = image.astype(np.float32) / 255.0
-    
+
     filename = os.path.basename(src_file)
     short_name = os.path.splitext(filename)[0]
-    pic_name = os.path.join(dst_dir, short_name + '.rgb')
+    pic_name = os.path.join(dst_dir, short_name + ".rgb")
     image.tofile(pic_name)
     print(f"write:{pic_name}")
 
